e9a7962044a1e41aefb6fedd02c6674dffa8c72b
younes692 <younes.moumou10@gmail.com>
2026-04-25 15:16:52 +0100

    Change heading text from 'Hello World' to 'Hello class'

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './index.css';
 export default function App() {
   return (
     <div className="container">
-      <h1>Hello World</h1>
+      <h1>Hello class</h1>
       <p>My first CI/CD pipeline</p>
     </div>
   );
