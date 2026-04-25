5da83907c04ee0336ce0d3d347810d1b8b34473c
younes692 <younes692@users.noreply.github.com>
2026-04-25 00:26:38 +0000

    fix: add missing ESLint, Prettier and gitignore config files
    .eslintrc.cjs and .prettierrc are required for npm run lint and
    npm run format:check to work in CI.

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true, node: true, jest: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
+};
